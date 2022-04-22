c38b3662e9db27fba08c7ddb8aa71fad70d4d3e7
Sen Feng <senfeng6@gmail.com>
2022-04-22 16:52:34 -0400

    tp2

--- pathfinding.py
+++ pathfinding.py
@@ -1,31 +1,33 @@
 import math
 
 # Watched: https://www.youtube.com/watch?v=-L-WgKMFuhE&ab_channel=SebastianLague
-#          https://www.youtube.com/watch?v=aKYlikFAV4k
 # Sources: http://theory.stanford.edu/~amitp/GameProgramming/AStarComparison.html#the-a-star-algorithm
 
 def pathSolution(maze, start, end, rows, cols):
-    start = (start[0], start[1], 0, None)
-    end = (end[0], end[1], 0, None)
+    startNode = (start[0], start[1], 0)
     toVisit = []
     visited = []
-    directionMap = dict()
-
-    toVisit.append(start)
+    toVisit.append(startNode)
+    directionMap = {startNode:None}
+    
     # Get the node with lowest f 
     while len(toVisit) > 0:
         current = toVisit[0]
-        for node in toVisit:
-            if node[2] < current[2]:
-                current = node
+        if len(toVisit) > 1:
+            for someNode in toVisit:
+                if someNode[2] < current[2]:
+                    current = someNode
         toVisit.remove(current)
-        visited.append(current)
-        if current == end:
+
+        # Find path
+        if current[0]==end[0] and current[1]==end[1]:
             path = []
-            while current != None:
+            while directionMap[current]:
                 path.append((current[0],current[1]))
-                current = directionMap[current[3]]
+                current = directionMap.get(current)
             return path
+
+        visited.append(current)
         # All potential directions: left, right, top, down
         directions = [(0,1),(1,0),(-1,0),(0,-1)]
         for (r,c) in directions: 
@@ -33,19 +35,12 @@ def pathSolution(maze, start, end, rows, cols):
             newCol = current[1] + c
             if (newRow >= 0 and newRow < rows and newCol >= 0 and newCol < cols):
                 if maze[newRow][newCol] == 0:
+                    # Calculate all the values
                     g = math.dist((start[0],start[1]),(newRow,newCol))
                     h = math.dist((end[0],end[1]),(newRow,newCol))
-                    f = g+h
-                    newNode = (newRow,newCol,f,current)
-                    check = False
-                    for node in toVisit:
-                        if node[0] == newRow and node[1] == newCol:
-                            directionMap[node] = current
-                            node = (newRow,newCol,f,current)
-                            check = True
-                    if check == False:
-                        toVisit.append(newNode)
-                        directionMap[newNode] = current
-
-
-    
+                    f = int((g+h)*10)
+                    newNode = (newRow,newCol,f)
+                    if newNode not in toVisit:
+                        if newNode not in visited:
+                            toVisit.append(newNode)
+                            directionMap[newNode] = (current[0],current[1],current[2])
